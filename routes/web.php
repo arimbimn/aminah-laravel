2c7d7059a868d4e21182c29c6eda2f90baf01d4a
donatseres <razka173@gmail.com>
2022-08-05 02:54:43 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BorrowerController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LenderController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +19,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/tentang-kami', [HomeController::class, 'about']);
+Route::get('/cara-kerja', [HomeController::class, 'how_to_work']);
+Route::get('/home', [LenderController::class, 'index']);
+Route::get('/mitra', [LenderController::class, 'mitra']);
+Route::get('/profile', [LenderController::class, 'profile']);
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/admin/login', [LoginController::class, 'login_admin']);
+Route::get('/forgot-password', [LoginController::class, 'forgot_password']);
+Route::get('/recovery-password', [LoginController::class, 'recovery_password']);
+Route::get('/borrower/profile', [BorrowerController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/borrower/register', [RegisterController::class, 'register_borrower']);
+Route::get('/dashboard', [AdminController::class, 'index']);
+Route::get('/data-pengajuan-masuk', [AdminController::class, 'data_pengajuan_masuk']);
+Route::get('/data-pengajuan-masuk/detail', [AdminController::class, 'detail_pengajuan_masuk']);
+Route::get('/rincian-pendanaan', [AdminController::class, 'rincian_pendanaan']);
+Route::get('/rincian-pendanaan/detail', [AdminController::class, 'detail_rincian_pendanaan']);
+Route::get('/data-mitra', [AdminController::class, 'data_mitra']);
+Route::get('/data-mitra/detail', [AdminController::class, 'detail_data_mitra']);
+Route::get('/data-keuangan', [AdminController::class, 'data_keuangan']);
+Route::get('/data-keuangan/detail', [AdminController::class, 'detail_keuangan']);
