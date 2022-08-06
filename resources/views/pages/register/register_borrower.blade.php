636898d24dca2cb76e81a6c73fc527b0708de430
donatseres <razka173@gmail.com>
2022-08-07 03:26:06 +0700

    add borrower registration

--- resources/views/pages/register/register_borrower.blade.php
+++ resources/views/pages/register/register_borrower.blade.php
@@ -1,135 +1,170 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aminah | Register Calon Mitra</title>
-
-    <!-- Favicons -->
-    <link href="{{ asset('') }}/img/Logo-Aminah-03.png" rel="icon">
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('') }}/admin/plugins/fontawesome-free/css/all.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('') }}/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('') }}/admin/dist/css/adminlte.min.css">
-
-    <style>
-        .register-page {
-            background-color: lightgreen;
-            background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
-            background-size: cover;
-            background-repeat: no-repeat;
-            position: relative;
-
-        }
-
-        .register-card-body,
-        .card {
-            border-radius: 20px;
-            margin-top: 20px;
-            margin-bottom: 10px;
-        }
-
-        .register-box {
-            width: 1000px;
-            
-        }
-    </style>
-
-</head>
-
-<body class="hold-transition register-page">
-    <div class="register-box">
-        <div class="card">
-            <div class="card-body register-card-body">
-                <div class="register-logo mt-0 mb-0">
-                    <a href="/">
-                        <img src="{{ asset('') }}/img/Aminah2.png" width="150" alt="">
+<x-registration-layout :title="$title">
+  <div class="wrapper">
+    <div class="content-wrapper">
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row justify-content-center">
+            <div class="col-lg-8">
+              {{-- BEGIN CARD --}}
+              <form action="/borrower/register" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="card card-yellow">
+                  {{-- BEGIN CARD HEADER --}}
+                  <div class="card-header">
+                    <div class="register-logo mt-0 mb-0">
+                      <a href="/">
+                        <img src="{{ asset('img') }}/Aminah2.png" width="150" alt="">
                         <h4 class="register-box-msg">SELAMAT DATANG CALON MITRA!</h4>
-                    </a>
-                </div>
-                <p class="login-box-msg">Harap isi data dibawah ini dengan benar</p>
-                <form class="row g-3" action="/borrower_register" method="post">
-                    <div class="col-md-6 mt-3">
-                        <label for="nama-umkm" class="form-label">Nama UMKM</label>
-                        <input type="text" name="nama-umkm" id="nama-umkm" class="form-control" placeholder="masukkan nama usaha anda disini">
+                      </a>
                     </div>
-                    <div class="col-md-6 mt-3">
-                        <label for="nama-pemilik" class="form-label">Nama Pemilik UMKM (sesuai KTP)</label>
-                        <input type="text" name="nama-pemilik" id="nama-pemilik" class="form-control" placeholder="masukkan nama lengkap anda disini">
+                  </div>
+                  {{-- END CARD HEADER --}}
+                  {{-- BEGIN CARD BODY --}}
+                  <div class="card-body">
+                    <p class="login-box-msg">Harap isi data dibawah ini dengan benar</p>
+                    {{-- BEGIN NAMA-UMKM --}}
+                    <div class="form-group">
+                      <x-basic.label for="umkmName" value="Nama UMKM" required="true" />
+                      <x-basic.input type="text" name="umkmName" :error="$errors->first('umkmName')" placeholder="masukkan nama usaha anda disini..." />
                     </div>
-                    <div class="col-6 mt-4">
-                        <label for="alamat-umkm" class="form-label">Alamat UMKM </label>
-                        <input type="text" name="alamat-umkm" class="form-control" id="alamat-umkm" placeholder="masukkan alamat usaha anda disini">
+                    {{-- EMD NAMA-UMKM --}}
+                    {{-- BEGIN NAMA PEMILIK --}}
+                    <div class="form-group">
+                      <x-basic.label for="fullName" value="Nama Pemilik UMKM (sesuai KTP)" required="true" />
+                      <x-basic.input type="text" name="fullName" :error="$errors->first('fullName')" placeholder="masukkan nama lengkap anda disini..." />
                     </div>
-                    <div class="col-6 mt-4">
-                        <label for="alamat-rumah" class="form-label">Alamat Rumah (sesuai KTP)</label>
-                        <input type="text" class="form-control" id="alamat-rumah" placeholder="masukkan alamat rumah anda disini">
+                    {{-- END NAMA PEMILIK --}}
+                    <div class="form-group">
+                      <x-basic.label for="alamat-umkm" value="Alamat UMKM" required="true" />
+                      <x-basic.input type="text" name="umkmAddress" :error="$errors->first('umkmAddress')" placeholder="masukkan alamat usaha anda disini..." />
                     </div>
-                    <div class="col-md-4 mt-4">
-                        <label for="input-nik" class="form-label">NIK (Nomor Induk Kependudukan)</label>
-                        <input type="text" name="input-nik" class="form-control" id="input-nik" placeholder="masukkan nomor NIK anda disini">
+                    <div class="form-group">
+                      <x-basic.label for="alamat-rumah" value="Alamat Rumah (sesuai KTP)" required="true" />
+                      <x-basic.input type="text" name="homeAddress" :error="$errors->first('homeAddress')" placeholder="masukkan alamat rumah anda disini..." />
                     </div>
-                    <div class="col-md-4 mt-4">
-                        <label for="input-no-hp" class="form-label">Nomor Telpon/HP Aktif</label>
-                        <input type="text" class="form-control" id="input-no-hp" placeholder="masukkan no. telpon/hp anda yang aktif disini">
+                    <div class="form-group">
+                      <x-basic.label for="input-nik" value="NIK (Nomor Induk Kependudukan)" required="true" />
+                      <x-basic.input type="text" name="nik" :error="$errors->first('nik')" placeholder="masukkan nomor NIK anda disini..." />
                     </div>
-                    <div class="col-md-4 mt-4">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" name="email" class="form-control" id="email" placeholder="masukkan alamat email kamu disini">
+                    <div class="form-group">
+                      <x-basic.label for="phoneNumber" value="Nomor Telpon/HP Aktif" required="true" />
+                      <x-basic.input type="text" name="phoneNumber" :error="$errors->first('phoneNumber')" placeholder="masukkan no. telpon/hp anda yang aktif disini..." />
                     </div>
-                    <div class="col-md-4 mt-4">
-                        <label for="input-rekening" class="form-label">Nomor Rekening</label>
-                        <input type="text" name="input-rekening" class="form-control" id="input-rekening" placeholder="masukkan nomor rekening anda disini">
+                    <div class="form-group">
+                      <x-basic.label for="email" value="Email" required="true" />
+                      <x-basic.input type="text" name="email" :error="$errors->first('email')" placeholder="masukkan alamat email kamu disini..." />
                     </div>
-                    <div class="col-4 mt-4">
-                        <label for="input-income" class="form-label">Pendapatan UMKM per Bulan</label>
-                        <input type="text" class="form-control" id="input-income" placeholder="masukkan jumlah pendapatan usaha kamu per bulan. mis : 10000">
+                    <div class="form-group">
+                      <x-basic.label for="accountNumber" value="Nomor Rekening" required="true" />
+                      <x-basic.input type="text" name="accountNumber" :error="$errors->first('accountNumber')" placeholder="masukkan nomor rekening anda disini..." />
                     </div>
-                    <div class="col-4 mt-4">
-                        <label for="input-pengajuan" class="form-label">Jumlah Pengajuan</label>
-                        <input type="text" class="form-control" id="input-pengajuan" placeholder="masukkan jumlah pengajuan kamu. mis : 10000">
+                    <div class="form-group">
+                      <x-basic.label for="income" class="form-label" value="Pendapatan UMKM per Bulan" required="true" />
+                      <x-basic.input type="text" name="income" :error="$errors->first('income')" placeholder="masukkan jumlah pendapatan usaha kamu per bulan. mis : 10000" />
                     </div>
-                    <div class="mb-3 col-6 mt-4">
-                        <label for="file-ktp" class="form-label">Foto KTP Pemilik UMKM (unggah foto KTP anda disini) </label>
-                        <input class="form-control" type="file" id="file-ktp">
+                    <div class="form-group">
+                      <x-basic.label for="amount" class="form-label" value="Jumlah Pengajuan" required="true" />
+                      <x-basic.input type="text" name="amount" :error="$errors->first('amount')" placeholder="masukkan jumlah pengajuan kamu. mis : 10000" />
                     </div>
-                    <div class="mb-3 col-6 mt-4">
-                        <label for="file-siu" class="form-label">Foto Surat Izin Usaha (SIU) (unggah foto siu disini)</label>
-                        <input class="form-control" type="file" id="file-siu">
-                    </div>
-                    <div class="mb-3 col-12 mt-4">
-                        <label for="file-foto-umkm" class="form-label">Foto UMKM (unggah foto usaha anda disini) </label>
-                        <input class="form-control" type="file" id="file-foto-umkm">
-                    </div>
-                    <div class="mb-3 col-12">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck">
-                            <label class="form-check-label" for="gridCheck">
-                                Saya menyatakan bahwa data yang saya masukan adalah benar
-                            </label>
+                    <div class="form-group">
+                      <label for="file-ktp" class="form-label">Foto KTP Pemilik UMKM (unggah foto KTP anda disini) </label>
+                      <input class="form-control" name="file-ktp" type="file" id="file-ktp">
+                      @error('file-ktp')
+                        <div class="text text-danger">
+                          <small>{{ $message }}</small>
                         </div>
+                      @enderror
                     </div>
-                    <div class="col">
-                        <button type="submit" class="btn btn-success">Daftarkan Usaha Saya Sekarang!</button>
+                    <div class="form-group">
+                      <label for="file-siu" class="form-label">Foto Surat Izin Usaha (SIU) (unggah foto siu disini)</label>
+                      <input class="form-control" name="file-siu" type="file" id="file-siu">
+                      @error('file-siu')
+                        <div class="text text-danger">
+                          <small>{{ $message }}</small>
+                        </div>
+                      @enderror
                     </div>
-                </form>
+                    <div class="form-group">
+                      <label for="file-foto-umkm" class="form-label">Foto UMKM (unggah foto usaha anda disini) </label>
+                      <input class="form-control" name="file-foto-umkm" type="file" id="file-foto-umkm">
+                      @error('file-foto-umkm')
+                        <div class="text text-danger">
+                          <small>{{ $message }}</small>
+                        </div>
+                      @enderror
+                    </div>
+                    <div class="form-group">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="approvedCheck" name="approvedCheck">
+                        <label class="form-check-label" for="approvedCheck">
+                          Saya menyatakan bahwa data yang saya masukan adalah benar
+                        </label>
+                      </div>
+                      @error('approvedCheck')
+                        <div class="text text-danger">
+                          <small>{{ $message }}</small>
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+                  {{-- END CARD BODY --}}
+                  {{-- BEGIN CARD FOOTER --}}
+                  <div class="card-footer">
+                    <div class="row d-flex justify-content-start mb-2">
+                      <div class="col-md-12 mb-0 d-flex d-none d-md-block d-lg-block mb-2">
+                        <button type="submit" class="btn btn-outline-success col-12"><i class="fa fa-check-square-o"></i> Daftarkan Usaha Saya Sekarang!</button>
+                      </div>
+                      <div class="col-md-12 mb-0 d-flex d-none d-md-block d-lg-block">
+                        <a href="{{ url()->previous() }}" class="btn btn-outline-secondary col-12"><i class="fa fa-undo"></i>
+                          Kembali</a>
+                      </div>
+                    </div>
+                  </div>
+                  {{-- END CARD FOOTER --}}
+                </div>
+              </form>
             </div>
+          </div>
         </div>
+      </section>
+    </div>
+  </div>
 
-        <!-- jQuery -->
-        <script src="{{ asset('') }}/admin/plugins/jquery/jquery.min.js"></script>
-        <!-- Bootstrap 4 -->
-        <script src="{{ asset('') }}/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <!-- AdminLTE App -->
-        <script src="{{ asset('') }}/admin/dist/js/adminlte.min.js"></script>
-        <!-- /.card -->
+  @push('page_css')
+    <style>
+      .content-wrapper {
+        background-color: lightgreen;
+        background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+        background-size: cover;
+        background-repeat: no-repeat;
+        position: relative;
+      }
 
-</body>
+      .register-card-body,
+      .card {
+        border-top-left-radius: 40px;
+        border-top-right-radius: 40px;
+        border-bottom-left-radius: 40px;
+        border-bottom-right-radius: 40px;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        padding-bottom: 30px;
+      }
 
-</html>
+      .card-header {
+        border-top-left-radius: 30px;
+        border-top-right-radius: 30px;
+      }
+    </style>
+  @endpush
+
+  @section('third_party_scripts')
+    <!-- jQuery -->
+    <script src="{{ asset('admin') }}/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('admin') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('admin') }}/dist/js/adminlte.min.js"></script>
+  @endsection
+
+</x-registration-layout>
