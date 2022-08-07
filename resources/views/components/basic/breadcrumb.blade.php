0da52e943d0d6bd0fbc026800f3fbf8eaf3751ea
donatseres <razka173@gmail.com>
2022-08-07 21:29:10 +0700

    add component

---
+++ resources/views/components/basic/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+@props(['href' => '#', 'active' => false, 'title' => 'Page'])
+
+<li class="breadcrumb-item @if ($active == 1) {{ 'active' }} @endif ">
+    @if ($active == 1)
+        {{ $title }}
+    @endif
+    @if (!$active == 0)
+        <a href="{{ $href }}">{{ $title }}</a>
+    @endif
+</li>
