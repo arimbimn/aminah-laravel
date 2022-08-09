e0b9bed3d61ade26c8b6c669d54deee4b393f965
donatseres <razka173@gmail.com>
2022-08-09 19:34:25 +0700

    update admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,26 +15,26 @@ class AdminController extends Controller
 
     public function rincian_pendanaan()
     {
-        return view('pages/admin/rincian_pendanaan', [
+        return view('pages/admin/pendanaan/index', [
             "title" => "Aminah | Rincian Pendanaan Aminah",
         ]);
     }
     public function detail_rincian_pendanaan()
     {
-        return view('pages/admin/detail_rincian_pendanaan', [
+        return view('pages/admin/pendanaan/detail', [
             "title" => "Aminah | Detail Rincian Pendanaan",
         ]);
     }
 
     public function data_keuangan()
     {
-        return view('pages/admin/data_keuangan', [
+        return view('pages/admin/keuangan/index', [
             "title" => "Aminah | Data Keuangan Aminah",
         ]);
     }
     public function detail_keuangan()
     {
-        return view('pages/admin/detail_data_keuangan', [
+        return view('pages/admin/keuangan/detail', [
             "title" => "Aminah | Detail Keuangan Aminah",
         ]);
     }
