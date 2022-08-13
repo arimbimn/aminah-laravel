f851f98e07b733d4596c56ef798f68aa3b65247c
donatseres <razka173@gmail.com>
2022-08-14 00:16:33 +0700

    add captcha to config

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // custom providers
+        Mews\Captcha\CaptchaServiceProvider::class, //captcha
+
     ],
 
     /*
@@ -210,6 +213,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+
+        // custom aliases
+        'Captcha' => Mews\Captcha\Facades\Captcha::class, //captcha
     ])->toArray(),
 
 ];
