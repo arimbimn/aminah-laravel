5a6d35fcd583a444978d9032b11f86b80b5088bb
donatseres <razka173@gmail.com>
2022-08-15 03:12:48 +0700

    update ui

--- resources/views/pages/admin/pendanaan/index.blade.php
+++ resources/views/pages/admin/pendanaan/index.blade.php
@@ -1,19 +1,16 @@
 @extends('layouts.admin.template_admin')
 
 @section('content')
-    <div class="content-wrapper">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h2>
-                        <br> Rincian Pendanaan Aminah
-                    </h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col mt-4">
-                    <div class="card">
-                        <table class="table table-hover">
+    <div class="container-fluid">
+        @include('layouts.admin.navigation')
+        <div class="row">
+            <div class="col mt-4">
+                <div class="card">
+                    <div class="card-header">
+                        <x-table.card-header tableName="{{ isset($tableName) ? $tableName : 'Tabel Data' }}" />
+                    </div>
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-wrap">
                             <thead>
                                 <tr>
                                     <th scope="col">No.</th>
@@ -25,15 +22,16 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php $i = 1; ?>
-                                <tr>
-                                    <th scope="row"><?= $i++ ?></th>
-                                    <td>*nama Borrower</td>
-                                    <td>*status*</td>
-                                    <td>*sisa Periode Pendanaan*</td>
-                                    <td>*Jumlah Total Pendanaan*</td>
-                                    <td><a href="/rincian-pendanaan/detail" class="btn btn-info"><i class="fa fa-eye"></i> Detail</a></td>
-                                </tr>
+                                @foreach ($records as $index => $record)
+                                    <tr>
+                                        <th scope="row">{{ $index + 1 }}</th>
+                                        <td>{{ $record->borrower->name }}</td>
+                                        <td>{{ $record->borrower->status }}</td>
+                                        <td>*sisa Periode Pendanaan*</td>
+                                        <td>Rp.{{ number_format($record->dana_terkumpul, 0, ',', '.') }},-</td>
+                                        <td><a href="/rincian-pendanaan/detail" class="btn btn-info"><i class="fa fa-eye"></i> Detail</a></td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
