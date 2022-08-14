19559d548010680bcc885cff3c2a9c1b5ab68842
donatseres <razka173@gmail.com>
2022-08-15 00:58:08 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BorrowerController;
 use App\Http\Controllers\HomeController;
@@ -76,6 +77,13 @@ Route::middleware('lender')->group(function () {
     Route::get('/lender/home', [LenderController::class, 'index'])->name('lender');
     Route::get('/lender/profile', [LenderController::class, 'profile'])->name('lender.profile');
     Route::get('/lender/mitra', [LenderController::class, 'mitra'])->name('lender.mitra');
+
+    Route::get('/lender/keranjang', [CartController::class, 'cartList'])->name('cart.list');
+    Route::post('/cart', [CartController::class, 'addToCart'])->name('cart.store');
+    Route::post('/update-cart', [CartController::class, 'updateCart'])->name('cart.update');
+    Route::post('/remove', [CartController::class, 'removeCart'])->name('cart.remove');
+    Route::post('/clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
+    Route::post('/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
 });
 
 Route::get('/', [HomeController::class, 'index']);
