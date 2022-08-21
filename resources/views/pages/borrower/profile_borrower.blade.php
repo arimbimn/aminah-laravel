2b16b1059f83c2e2cb74d4da873979266f4d462a
arimbi <arimbimega1@gmail.com>
2022-08-21 12:35:27 +0700

    modify view borrower profile

--- resources/views/pages/borrower/profile_borrower.blade.php
+++ resources/views/pages/borrower/profile_borrower.blade.php
@@ -14,97 +14,103 @@
 
         <div class="row mb-4">
             <div class="col-lg-6 col-12">
-                <a href="/" class="btn btn-outline-success mb-2 col-12"> Tarik Saldo</a>
+                <a href="/mitra/profile" class="btn btn-outline-success mb-2 col-12"> Tarik Saldo</a>
             </div>
             <div class="col-lg-6 col-12">
-                <a href="/mitra/profile/ajukan-pendanaan" class="btn btn-outline-warning mb-2 col-12"> Ajukan Pendanaan</a>
+                <a href="/mitra/profile/ajukan-pendanaan" class="btn btn-outline-warning mb-2 col-12" {{ $user->borrower->status == 'pending' ? 'disabled' : '' }}> Ajukan Pendanaan</a>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-lg-3 pt-4 pt-lg-0 order-1 order-lg-1 content">
-                <img src="{{ asset('') }}/img/testimonials.jpg" class="img-thumbnail" alt="lender" width="200">
+            <div class="col-lg-6 pt-4 pt-lg-0 order-1 order-lg-1 content">
+                <img src="{{ asset('') }}/img/{{ isset($user->borrower) ? $user->borrower->business_image : 'user.png' }}" class="img-thumbnail" alt="{{ isset($user->borrower) ? $user->borrower->business_name : '-' }}" width="200">
                 <h4><b>{{ isset($user->borrower) ? $user->borrower->business_name : '-' }}</b></h4>
-                <h5>Nama Pemilik : {{ $user->name }}</h5>
-                <h5>Email : {{ $user->email }}</h5>
-                <h5>HP : {{ isset($user->borrower) ? $user->borrower->phone_number : '-' }}</h5>
+                <dl class="row">
+                    <dt class="col-sm-5">Nama Pemilik Usaha</dt>
+                    <dd class="col-sm-7">: {{ $user->name }}</dd>
+
+                    <dt class="col-sm-5">Email</dt>
+                    <dd class="col-sm-7">: {{ $user->email }}</dd>
+                    
+                    <dt class="col-sm-5">No. HP</dt>
+                    <dd class="col-sm-7">: {{ isset($user->borrower) ? $user->borrower->phone_number : '-' }}</dd>
+                    
+                    <dt class="col-sm-5">Nama Pemilik Rekening</dt>
+                    <dd class="col-sm-7">: {{ isset($user->borrower) ? $user->borrower->account_name : '-' }}</dd>
+
+                    <dt class="col-sm-5">Nama Bank</dt>
+                    <dd class="col-sm-7">: {{ isset($user->borrower) ? $user->borrower->bank_name : '-' }}</dd>
+
+                    <dt class="col-sm-5">No. Rekening</dt>
+                    <dd class="col-sm-7">: {{ isset($user->borrower) ? $user->borrower->account_number : '-' }}</dd>
+                </dl>
             </div>
-            <div class="card col-lg-5 order-2 order-lg-2 border-dark mb-3" style="max-width: 50rem;">
-                <div class="card-header">Ringkasan</div>
+            <div class="card col-lg-6 order-2 order-lg-2 border-dark mb-3" style="max-width: 50rem;">
+                <div class="card-header">Ringkasan Pengajuan</div>
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="card-body text-dark">
-                            <h5 class="card-title">Dana Pengajuan Awal</h5>
+                            <p class="card-title"><b>Dana Pengajuan Awal</b></p>
                             <p class="card-text">Rp {{ isset($user->borrower) ? $user->borrower->borrower_first_submission : '-' }}</p>
 
-                            <h5 class="card-title">Pendanan Lunas</h5>
+                            <p class="card-title"><b>Dana Disetujui</b></p>
                             <p class="card-text">Rp 0</p>
+                            
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="card-body text-dark">
-                            <h5 class="card-title">Dana Disetujui</h5>
+                            <p class="card-title"><b>Pendanan Lunas</b></p>
                             <p class="card-text">Rp 0</p>
 
-                            <h5 class="card-title">Pendanaan Belum Lunas</h5>
+                            <p class="card-title"><b>Pendanaan Belum Lunas</b></p>
                             <p class="card-text">Rp 0</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="card-body text-dark">
+                            <p class="card-title"><b>Status Pengajuan</b></p>
+                            <p class="card-text">{{ isset($user->borrower) ? $user->borrower->status : '-' }}</p>
+
+                            <p class="card-title"><b>Jangka Waktu Pengajuan</b></p>
+                            <p class="card-text">{{ isset($user->borrower) ? $user->borrower->duration : '-' }}</p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card-body text-dark">
+                            <p class="card-title"><b>Estimasi Bagi Hasil</b></p>
+                            <p class="card-text">{{ isset($user->borrower) ? $user->borrower->profit_sharing_estimate : '-' }}</p>
+
+                            <p class="card-title"><b>Jumlah Lender Terkumpul</b></p>
+                            <p class="card-text">-</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card-body text-dark">
+                            <p class="card-title"><b>Tujuan Pengajuan</b></p>
+                            <p class="card-text">{{ isset($user->borrower) ? $user->borrower->purpose : '-' }}</p>
+                            
+                        </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 pt-4 pt-lg-0 order-3 order-lg-3 content mb-4">
-                <dl class="row">
-                    <dt class="col-sm-6">Nama Pemilik Rekening</dt>
-                    <dd class="col-sm-6">{{ isset($user->borrower) ? $user->borrower->account_name : '-' }}</dd>
-
-                    <dt class="col-sm-6">Nama Bank</dt>
-                    <dd class="col-sm-6">{{ isset($user->borrower) ? $user->borrower->bank_name : '-' }}</dd>
-
-                    <dt class="col-sm-6">No. Rekening</dt>
-                    <dd class="col-sm-6">{{ isset($user->borrower) ? $user->borrower->account_number : '-' }}</dd>
-                    <hr>
-                    <div class="col mt-5">
-                        <button class="btn btn-success">Tarik Saldo</button>
-                    </div>
-
-                    <div class="col mt-5">
-                        <a href="/mitra/profile/ajukan-pendanaan" class="btn btn-warning"> Ajukan Pendanaan</a>
-                    </div>
-                </dl>
-            </div>
+            
         </div>
         <hr>
-        @if ($user->borrower)
-            <div class="row mt-4">
-                {{-- TABEL PENGAJUAN --}}
-                <div class="col">
-                    <h4>Tabel Data Pengajuan</h4>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">No.</th>
-                                <th scope="col">Status Pengajuan</th>
-                                <th scope="col">Tujuan Pengajuan</th>
-                                <th scope="col">Jangka Waktu Pengajuan</th>
-                                <th scope="col">Estimasi Bagi Hasil</th>
-                                <th scope="col">Nama Lender</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php $i = 1; @endphp
-                            <tr>
-                                <th>@php $i++ @endphp</th>
-                                <td>{{ isset($user->borrower) ? $user->borrower->status : '-' }}</td>
-                                <td>{{ isset($user->borrower) ? $user->borrower->purpose : '-' }}</td>
-                                <td>{{ isset($user->borrower) ? $user->borrower->duration : '-' }}</td>
-                                <td>{{ isset($user->borrower) ? $user->borrower->profit_sharing_estimate : '-' }}</td>
-                                <td> - </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        @endif
 
         @if ($user->borrower)
             {{-- TABEL PENDANAAN --}}
