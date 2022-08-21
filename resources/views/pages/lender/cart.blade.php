579383f11cfe3512f1b3daf9c5026bf757af1c39
donatseres <razka173@gmail.com>
2022-08-21 21:34:52 +0700

    fix bugs

--- resources/views/pages/lender/cart.blade.php
+++ resources/views/pages/lender/cart.blade.php
@@ -1,162 +1,170 @@
 @extends('layouts.user.lender.template')
 
 @section('content')
-    <!-- ======= Breadcrumbs ======= -->
-    <div class="breadcrumbs" data-aos="fade-in">
-        <div class="container">
-            <h2>Beranda</h2>
-            <p>Selamat datang di beranda kamu, {{ Auth::user()->name }} !</p>
-        </div>
-    </div><!-- End Breadcrumbs -->
-
-    <div class="container mt-4">
-        {{--  --}}
-        <div class="row">
-            <div class="col-12">
-                @if ($message = Session::get('success'))
-                    <div class="card bg-success">
-                        <div class="card-header">
-                            <h5 class="text text-bold text-white">{{ $message }}</h5>
-                        </div>
-                    </div>
-                @endif
-                @if ($message = Session::get('error'))
-                    <div class="card bg-warning">
-                        <div class="card-header">
-                            <h5 class="text text-bold">{{ $message }}</h5>
-                        </div>
-                    </div>
-                @endif
-            </div>
-        </div>
-        {{--  --}}
-        <div class="row">
-            <div class="col-12">
-                <div class="card mb-5 mt-5">
-                    <div class="card-header">
-                        <h3 class="card-title">Cart List</h3>
-                    </div>
-                    {{-- BEGIN CARD BODY --}}
-                    <div class="card-body">
-                        
-                        <table class="table table-responsive" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th class="text-left">Nama</th>
-                                    <th class="pl-5 text-left lg:text-right lg:pl-0">
-                                        <span class="hidden lg:inline">Tujuan Pengajuan</span>
-                                    </th>
-                                    <th class="pl-5 text-left lg:text-right lg:pl-0">
-                                        <span class="hidden lg:inline">Rincian</span>
-                                    </th>
-                                    <th class="pl-5 text-left lg:text-right lg:pl-0">
-                                        <span class="hidden lg:inline">Jumlah Unit</span>
-                                    </th>
-                                    <th class="hidden text-right md:table-cell"> Harga</th>
-                                    <th class="hidden text-right md:table-cell"> Hapus </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($cartItems as $item)
-                                    <tr>
-                                        <td class="hidden pb-4 md:table-cell">
-                                            <a href="#">
-                                                <img src="{{ $item->attributes->image }}" class="w-20 rounded" alt="Thumbnail">
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a href="/admin/mitra/detail/{{ $item->id }}">
-                                                <p class="mb-2 md:ml-4">{{ $item->name }}</p>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <p class=" mb-2 md:ml-4">*isinya tujuan pengajuan yg ada 
-                                                di form waktu borrower isi buat ajukan pendanaan*</p>
-                                        </td>
-                                        <td>
-                                            <p class=" mb-2 md:ml-4">
-                                                <dl class="row">
-                                
-                                                    <dt class="col-sm-6"><small>Estimasi Bagi hasil</small></dt>
-                                                    <dd class="col-sm-6">5 %</dd>
-                                
-                                                    <dt class="col-sm-6"><small>Lama pendanaan</small></dt>
-                                                    <dd class="col-sm-6">12 bulan</dd>
-                                
-                                                    <dt class="col-sm-6"><small>Siklus bagi hasil</small></dt>
-                                                    <dd class="col-sm-6">Per 1 bulan</dd>
-                                
-                                                    <dt class="col-sm-6"><small>Periode bagi hasil</small></dt>
-                                                    <dd class="col-sm-6">12 bulan</dd>
-                                
-                                                    <dt class="col-sm-6"><small>Jaminan</small></dt>
-                                                    <dd class="col-sm-6">*BPKB Motor*</dd>
-                                
-                                                    <dt class="col-sm-6"><small>Akad</small></dt>
-                                                    <dd class="col-sm-6">-</dd>
-                                                </dl>
-                                            </p>
-                                        </td>
-                                        <td class="justify-center mt-6 md:justify-end md:flex">
-                                            <div class="h-10 w-28">
-                                                <div class="relative flex flex-row w-full h-8">
-                                                    <form action="{{ route('cart.update') }}" method="POST">
-                                                        @csrf
-                                                        <input type="hidden" name="id" value="{{ $item->id }}">
-                                                        <input min="1" type="number" name="quantity" value="{{ $item->quantity }}" class="text-center form-inline" />
-                                                        <button type="submit" class="btn btn-primary mt-3"><i class="fa fa-refresh"></i> Update</button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td class="hidden text-right md:table-cell">
-                                            <span class="text-sm font-medium lg:text-base">
-                                                Rp.{{ number_format($item->price, 0, ',', '.') }},-
-                                            </span>
-                                        </td>
-                                        <td class="hidden text-right md:table-cell">
-                                            <form action="{{ route('cart.remove') }}" method="POST">
-                                                @csrf
-                                                <input type="hidden" value="{{ $item->id }}" name="id">
-                                                <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                <tr>
-                                    <td colspan="2"></td>
-                                    <td>
-                                        Total jumlah unit: {{ Cart::session(Auth::user()->id)->getTotalQuantity() }}
-                                    </td>
-                                    <td colspan="2">
-                                        Total: Rp.{{ number_format(Cart::getTotal(), 0, ',', '.') }},-
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    {{-- END CARD BODY --}}
-                    {{-- BEGIN CARD FOOTER --}}
-                    <div class="card-footer">
-                        <div class="row">
-                            <div class="col-6">
-                                <form action="{{ route('cart.clear') }}" method="POST">
-                                    @csrf
-                                    <button class="btn btn-outline-danger col-12"><i class="fa fa-trash"></i> Remove All Cart</button>
-                                </form>
-                            </div>
-                            <div class="col-6">
-                                <form action="{{ route('cart.checkout') }}" method="POST">
-                                    @csrf
-                                    <button class="btn btn-outline-success col-12"><i class="fa fa-shopping-cart"></i> Checkout</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- END CARD FOOTER --}}
-                </div>
-            </div>
-        </div>
+  <!-- ======= Breadcrumbs ======= -->
+  <div class="breadcrumbs" data-aos="fade-in">
+    <div class="container">
+      <h2>Beranda</h2>
+      <p>Selamat datang di beranda kamu, {{ Auth::user()->name }} !</p>
     </div>
+  </div><!-- End Breadcrumbs -->
+
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-12">
+        @if ($message = Session::get('success'))
+          <div class="card bg-success">
+            <div class="card-header">
+              <h5 class="text text-bold text-white">{{ $message }}</h5>
+            </div>
+          </div>
+        @endif
+        @if ($message = Session::get('error'))
+          <div class="card bg-warning">
+            <div class="card-header">
+              <h5 class="text text-bold">{{ $message }}</h5>
+            </div>
+          </div>
+        @endif
+      </div>
+    </div>
+    {{--  --}}
+    <div class="row">
+      <div class="col-12">
+        <div class="card mb-5 mt-5">
+          <div class="card-header">
+            <h3 class="card-title">Cart List</h3>
+          </div>
+          {{-- BEGIN CARD BODY --}}
+          <div class="card-body">
+            <table class="table table-responsive" cellspacing="0">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th class="text-left">Nama</th>
+                  <th class="pl-5 text-left">
+                    <span>Tujuan Pengajuan</span>
+                  </th>
+                  <th class="pl-5 text-center">
+                    <span>Rincian</span>
+                  </th>
+                  <th class="pl-5 text-left">
+                    <span>Jumlah Unit</span>
+                  </th>
+                  <th> Harga</th>
+                  <th> Hapus </th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($cartItems as $item)
+                  <tr>
+                    <td class="hidden pb-4 md:table-cell">
+                      <a href="#">
+                        <img class="product-image" src="{{ $item->attributes->image }}" class="w-20 rounded" alt="Thumbnail" width="100%">
+                      </a>
+                    </td>
+                    <td>
+                      <a href="/lender/mitra/detail/{{ $item->id }}">
+                        <p class="mb-2">{{ $item->name }}</p>
+                      </a>
+                    </td>
+                    <td>
+                      <p class=" mb-2">*isinya tujuan pengajuan yg ada
+                        di form waktu borrower isi buat ajukan pendanaan*</p>
+                    </td>
+                    <td>
+                      <p class=" mb-2">
+                      <dl class="row">
+
+                        <dt class="col-sm-6"><small>Estimasi Bagi hasil</small></dt>
+                        <dd class="col-sm-6">5 %</dd>
+
+                        <dt class="col-sm-6"><small>Lama pendanaan</small></dt>
+                        <dd class="col-sm-6">12 bulan</dd>
+
+                        <dt class="col-sm-6"><small>Siklus bagi hasil</small></dt>
+                        <dd class="col-sm-6">Per 1 bulan</dd>
+
+                        <dt class="col-sm-6"><small>Periode bagi hasil</small></dt>
+                        <dd class="col-sm-6">12 bulan</dd>
+
+                        <dt class="col-sm-6"><small>Jaminan</small></dt>
+                        <dd class="col-sm-6">*BPKB Motor*</dd>
+
+                        <dt class="col-sm-6"><small>Akad</small></dt>
+                        <dd class="col-sm-6">-</dd>
+                      </dl>
+                      </p>
+                    </td>
+                    <td class="justify-center mt-6 md:justify-end md:flex">
+                      <div class="h-10 w-28">
+                        <div class="relative flex flex-row w-full h-8">
+                          <form action="{{ route('cart.update') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="id" value="{{ $item->id }}">
+                            <input min="1" type="number" name="quantity" value="{{ $item->quantity }}" class="text-center form-inline" />
+                            <button type="submit" class="btn btn-primary mt-3"><i class="fa fa-refresh"></i> Update</button>
+                          </form>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="hidden text-right md:table-cell">
+                      <span class="text-sm font-medium lg:text-base">
+                        Rp.{{ number_format($item->price, 0, ',', '.') }},-
+                      </span>
+                    </td>
+                    <td class="hidden text-right md:table-cell">
+                      <form action="{{ route('cart.remove') }}" method="POST">
+                        @csrf
+                        <input type="hidden" value="{{ $item->id }}" name="id">
+                        <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                      </form>
+                    </td>
+                  </tr>
+                @endforeach
+                <tr>
+                  <td colspan="2"></td>
+                  <td>
+                    Total jumlah unit: {{ Cart::session(Auth::user()->id)->getTotalQuantity() }}
+                  </td>
+                  <td colspan="2">
+                    Total: Rp.{{ number_format(Cart::getTotal(), 0, ',', '.') }},-
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {{-- END CARD BODY --}}
+          {{-- BEGIN CARD FOOTER --}}
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-6">
+                <form action="{{ route('cart.clear') }}" method="POST">
+                  @csrf
+                  <button class="btn btn-outline-danger col-12"><i class="fa fa-trash"></i> Remove All Cart</button>
+                </form>
+              </div>
+              <div class="col-6">
+                <form action="{{ route('cart.checkout') }}" method="POST">
+                  @csrf
+                  <button class="btn btn-outline-success col-12"><i class="fa fa-shopping-cart"></i> Checkout</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          {{-- END CARD FOOTER --}}
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
+
+@push('page_scripts')
+  <script>
+    $(document).ready(function() {
+      $(".product-image").on("error", function() {
+        $(this).attr('src', 'https://via.placeholder.com/1080x720.png?text=Business%20Image');
+      });
+    });
+  </script>
+@endpush
--- resources/views/pages/lender/mitra/detail.blade.php
+++ resources/views/pages/lender/mitra/detail.blade.php
@@ -1,71 +1,72 @@
 @extends('layouts.user.lender.template')
 
 @section('content')
-    <div class="breadcrumbs" data-aos="fade-in">
-        <div class="container">
-            <h2>{{ $funding->borrower->business_name }}</h2>
-            <p>*jenis mitra*</p>
-        </div>
+  <div class="breadcrumbs" data-aos="fade-in">
+    <div class="container">
+      <h2>{{ isset($funding->borrower->business_name) ? $funding->borrower->business_name : 'Usaha UMKM' }}</h2>
+      <p>{{ isset($funding->borrower->buniness_type) ? $funding->borrower->buniness_type : 'Lain-lain' }}</p>
     </div>
-    <!-- End Breadcrumbs -->
+  </div>
+  <!-- End Breadcrumbs -->
 
-    <div class="container mt-4 mb-4">
-        <div class="row">
-            <div class="col-6">
-                <img src="{{ asset('') }}/img/aminahImg1.jpg" class="img-thumbnail" alt="lender">
-                <h4><b>{{ isset($funding->borrower) ? $funding->borrower->business_name : '-' }}</b></h4>
-                <h5>Alamat UMKM : {{ isset($funding->borrower) ?  $funding->borrower->business_address :'-' }}</h5>
-                <h5>Deskripsi & Jenis UMKM</h5>
-                <p> Jenis UMKM : {{ isset($funding->borrower) ?  $funding->borrower->description : '-' }}</p>
-            </div>
-            <div class="col-6">
-                <h3><b>Rincian Pendanaan dari {{ $funding->borrower->business_name }}</b></h3>
-                <h5><small>*x* hari lagi</small></h5>
-                <h5>terkumpul {{ $funding->dana_terkumpul_persen }}%</h5>
-                <h5>terkumpul Rp.{{ $funding->dana_terkumpul }} dari Rp. {{ $funding->accepted_fund }}</h5>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width:{{ $funding->dana_terkumpul_persen }}%;" aria-valuenow="{{ $funding->dana_terkumpul_persen }}" aria-valuemin="0" aria-valuemax="100">{{ $funding->dana_terkumpul_persen }} %</div>
-                </div>
-                <hr>
-                <dl class="row">
-                    <dt class="col-sm-6">Nilai per unit pendanaan</dt>
-                    <dd class="col-sm-6">Rp 100.000</dd>
-
-                    <dt class="col-sm-6">Estimasi Bagi hasil</dt>
-                    <dd class="col-sm-6">{{ $funding->borrower->profit_sharing_estimate}}%</dd>
-
-                    <dt class="col-sm-6">Lama pendanaan</dt>
-                    <dd class="col-sm-6">{{ $funding->borrower->duration }}</dd>
-
-                    <dt class="col-sm-6">Siklus bagi hasil</dt>
-                    <dd class="col-sm-6">Per 1 bulan</dd>
-
-                    <dt class="col-sm-6">Periode bagi hasil</dt>
-                    <dd class="col-sm-6">{{ $funding->borrower->duration }}</dd>
-
-                    <dt class="col-sm-6">Jaminan</dt>
-                    <dd class="col-sm-6">*BPKB Motor*</dd>
-
-                    <dt class="col-sm-6">Akad</dt>
-                    <dd class="col-sm-6">-</dd>
-                </dl>
-
-                <hr>
-
-                <h4>Danai sebesar:</h4>
-                <h3><b>Rp100.000</b></h3>
-                <p>Estimasi bagi hasil: {{ $funding->borrower->profit_sharing_estimate}} %. Sisa *x* unit</p>
-                <div class="col-md-12 mb-0 d-flex d-none d-md-block d-lg-block mb-2">
-                    <form action="/cart" method="post">
-                        <input type="hidden" value="{{ $funding->borrower->id }}" name="id">
-                        <input type="hidden" value="{{ $funding->borrower->business_name }}" name="name">
-                        <input type="hidden" value="100000" name="price">
-                        <input type="hidden" value="{{ $funding->borrower->business_image }}" name="image">
-                        <input type="hidden" value="1" name="quantity">
-                        <button type="submit" class="btn btn-outline-success col-12"><i class="fa fa-check-square" {{ $funding->borrower->dana_terkumpul_persen == 100 ? 'disabled' : '' }}></i> Danai mitra sekarang!</button>
-                    </form>
-                </div>
-            </div>
+  <div class="container mt-4 mb-4">
+    <div class="row">
+      <div class="col-6">
+        <img src="{{ asset('') }}/img/aminahImg1.jpg" class="img-thumbnail" alt="lender">
+        <h4><b>{{ isset($funding->borrower->business_name) ? $funding->borrower->business_name : 'UMKM' }}</b></h4>
+        <h5>Alamat UMKM : {{ isset($funding->borrower->business_address) ? $funding->borrower->business_address : '-' }}</h5>
+        <h5>Deskripsi & Jenis UMKM</h5>
+        <p> {{ isset($funding->borrower->description) ? $funding->borrower->description : '-' }}</p>
+        <h6>Jenis UMKM: {{ isset($funding->borrower->buniness_type) ? $funding->borrower->buniness_type : 'Lain-lain' }}</h6>
+      </div>
+      <div class="col-6">
+        <h3><b>Rincian Pendanaan dari {{ $funding->borrower->business_name }}</b></h3>
+        <h5><small>*x* hari lagi</small></h5>
+        <h5>terkumpul {{ $funding->dana_terkumpul_persen }}%</h5>
+        <h5>terkumpul Rp.{{ $funding->dana_terkumpul }} dari Rp. {{ $funding->accepted_fund }}</h5>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" style="width:{{ $funding->dana_terkumpul_persen }}%;" aria-valuenow="{{ $funding->dana_terkumpul_persen }}" aria-valuemin="0" aria-valuemax="100">{{ $funding->dana_terkumpul_persen }} %</div>
         </div>
+        <hr>
+        <dl class="row">
+          <dt class="col-sm-6">Nilai per unit pendanaan</dt>
+          <dd class="col-sm-6">Rp 100.000</dd>
+
+          <dt class="col-sm-6">Estimasi Bagi hasil</dt>
+          <dd class="col-sm-6">{{ $funding->borrower->profit_sharing_estimate }}%</dd>
+
+          <dt class="col-sm-6">Lama pendanaan</dt>
+          <dd class="col-sm-6">{{ $funding->borrower->duration }}</dd>
+
+          <dt class="col-sm-6">Siklus bagi hasil</dt>
+          <dd class="col-sm-6">Per 1 bulan</dd>
+
+          <dt class="col-sm-6">Periode bagi hasil</dt>
+          <dd class="col-sm-6">{{ $funding->borrower->duration }}</dd>
+
+          <dt class="col-sm-6">Jaminan</dt>
+          <dd class="col-sm-6">*BPKB Motor*</dd>
+
+          <dt class="col-sm-6">Akad</dt>
+          <dd class="col-sm-6">-</dd>
+        </dl>
+
+        <hr>
+
+        <h4>Danai sebesar:</h4>
+        <h3><b>Rp100.000</b></h3>
+        <p>Estimasi bagi hasil: {{ $funding->borrower->profit_sharing_estimate }} %. Sisa *x* unit</p>
+        <div class="col-md-12 mb-0 d-flex d-none d-md-block d-lg-block mb-2">
+          <form action="/cart" method="post">
+            <input type="hidden" value="{{ $funding->borrower->id }}" name="id">
+            <input type="hidden" value="{{ $funding->borrower->business_name }}" name="name">
+            <input type="hidden" value="100000" name="price">
+            <input type="hidden" value="{{ $funding->borrower->business_image }}" name="image">
+            <input type="hidden" value="1" name="quantity">
+            <button type="submit" class="btn btn-outline-success col-12"><i class="fa fa-check-square" {{ $funding->borrower->dana_terkumpul_persen == 100 ? 'disabled' : '' }}></i> Danai mitra sekarang!</button>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
 @endsection
