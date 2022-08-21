cd0f916a2cfa636104f10381629ac21df243e020
arimbi <arimbimega1@gmail.com>
2022-08-21 12:34:28 +0700

    add method logout borrower

--- resources/views/layouts/user/borrower/borrower_navbar.blade.php
+++ resources/views/layouts/user/borrower/borrower_navbar.blade.php
@@ -6,7 +6,15 @@
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-        <a href="/" class="get-started-btn">Keluar</a>
-
+        <nav id="navbar" class="navbar order-last order-lg-0">
+            <li class="dropdown"><a href="#">{{ Auth::user()->name }}<i class="bi bi-chevron-down"></i></a>
+                <ul>
+                    <li><a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown">Logout<i class="fa fa-sign-out"></i></a></li>
+                    <form id="logout-form" action="@if (Route::has('logout')) {{ route('logout') }} @endif" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </ul>
+            </li>
+        </nav>
     </div>
 </header><!-- End Header -->
