2ae9aa7912fd8ccea90ab73f920eb3aa84e12f37
donatseres <razka173@gmail.com>
2022-08-22 04:04:44 +0700

    update funding table

--- database/migrations/2022_08_04_123709_create_funding_lender.php
+++ database/migrations/2022_08_04_123709_create_funding_lender.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('funding_id');
             $table->foreignId('lender_id');
+            $table->string('unit_trx')->nullable();
+            $table->float('amount')->nullable();
             $table->string('status')->nullable();
             $table->timestamps();
             $table->softDeletes();
