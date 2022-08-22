a13c3f4742024c151be8839ea51063ac6a3283b9
donatseres <razka173@gmail.com>
2022-08-22 19:38:28 +0700

    fix bug

--- resources/views/layouts/user/notification.blade.php
+++ resources/views/layouts/user/notification.blade.php
@@ -3,7 +3,7 @@
     <p class="alert alert-warning text text-center">Profil kamu belum lengkap, lengkapi profil kamu <a href="/lender/profile/edit"><strong>disini</strong></a></p>
   </div>
 @endif
-@if (Auth::user()->checkWaiting)
+@if (Auth::user()->checkWaiting->count() > 0)
   <div class="row">
     <p class="alert alert-danger text text-center">Ada pembayaran belum selesai, silahkan selesaikan pembayaran kamu <a href="/lender/dompet/bayar"><strong>disini</strong></a></p>
   </div>
