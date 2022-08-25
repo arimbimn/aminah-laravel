8aaee9ab78be6c1a3d29428d1af5594c358ecb12
arimbi <arimbimega1@gmail.com>
2022-08-25 20:56:21 +0700

    update view navbar borrower

--- resources/views/layouts/user/borrower/navbar.blade.php
+++ resources/views/layouts/user/borrower/navbar.blade.php
@@ -2,19 +2,20 @@
 <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-        <h1 class="logo me-auto"><a href="#">Aminah</a></h1>
+        <h1 class="logo me-auto"><a href="/mitra/profile">Aminah</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
         <nav id="navbar" class="navbar order-last order-lg-0">
-            <li class="dropdown"><a href="#">{{ Auth::user()->name }}<i class="bi bi-chevron-down"></i></a>
-                <ul>
-                    <li><a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown">Logout<i class="fa fa-sign-out"></i></a></li>
-                    <form id="logout-form" action="@if (Route::has('logout')) {{ route('logout') }} @endif" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </ul>
-            </li>
+            <ul>
+                <li class="dropdown"><a href="#">{{ Auth::user()->name }}<i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown">Logout<i class="fa fa-sign-out"></i></a></li>
+                        <form id="logout-form" action="@if (Route::has('logout')) {{ route('logout') }} @endif" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </ul>
+                </li>
         </nav>
     </div>
 </header><!-- End Header -->
