58248d2aef01eac22cfed3782155a1eeb02a417d
donatseres <razka173@gmail.com>
2022-08-29 20:37:07 +0700

    add fundinglender relationship

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -35,4 +35,9 @@ class Transaction extends Model
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
+
+    public function fundingLender()
+    {
+        return $this->hasOne(FundingLender::class);
+    }
 }
