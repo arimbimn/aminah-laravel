60c645408bbf5025dca7d0603986c92a64456f8a
donatseres <razka173@gmail.com>
2022-09-01 15:56:09 +0700

    rename login view

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        return view('pages.login.user');
+        return view('pages.login.index');
     }
     public function login_admin()
     {
